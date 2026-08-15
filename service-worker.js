dc298a5aa6dd9034feafa9a4d49cca0c90201ef4
bradjtrimble <bradjtrimble@gmail.com>
2026-08-15 09:54:06 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,56 +1,34 @@
-const CACHE_NAME = "little-realm-phone-v4";
+const CACHE_NAME = "little-realm-stable-v5";
 const FILES = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
   "./icon-192.png",
   "./icon-512.png",
-  "./house_A.png",
-  "./house_B.png"
+  "./terrain-seamless.png",
+  "./house-a.png",
+  "./house-b.png"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      ))
-      .then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put("./index.html", copy));
-          return response;
-        })
-        .catch(() => caches.match("./index.html"))
-    );
+  if(event.request.method !== "GET") return;
+  if(event.request.mode === "navigate"){
+    event.respondWith(fetch(event.request).then(response => {
+      const copy=response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put("./index.html",copy));
+      return response;
+    }).catch(() => caches.match("./index.html")));
     return;
   }
-
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-      return fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      });
-    })
-  );
+  event.respondWith(fetch(event.request).then(response => {
+    const copy=response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request,copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
 });
